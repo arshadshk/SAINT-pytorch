4518eeb6cf357b41b658b58206796ad72a722727
arshadshk7 <arshaikh5775@gmail.com>
2020-12-14 18:04:58 +0530

    todo dropout, documentation

--- saint.py
+++ saint.py
@@ -19,20 +19,20 @@ class Feed_Forward_block(nn.Module):
         
 
 class Encoder_block(nn.Module):
+    """
+    M = SkipConct(Multihead(LayerNorm(Qin;Kin;Vin)))
+    O = SkipConct(FFN(LayerNorm(M)))
+    """
 
     def __init__(self , dim_model, heads_en, total_ex ,total_cat):
         super().__init__()
-
-        # embedings  q,k,v = E = exercise ID embedding, category embedding, and positionembedding.
-        self.embd_ex =   nn.Embedding( total_ex , embedding_dim = dim_model )
+        self.embd_ex =   nn.Embedding( total_ex , embedding_dim = dim_model )                   # embedings  q,k,v = E = exercise ID embedding, category embedding, and positionembedding.
         self.embd_cat =  nn.Embedding( total_cat, embedding_dim = dim_model )
         #embd_pos =  nn.Embedding( total_pos, embedding_dim = dim_model )
-
-        # multihead attention
-        self.multi_en = nn.MultiheadAttention( embed_dim= dim_model, num_heads= heads_en,  )       ## todo add dropout, LayerNORM
-
-        # feedforward block 
-        self.ffn_en = Feed_Forward_block( dim_model )                                              ## todo dropout, LayerNorm
+        self.multi_en = nn.MultiheadAttention( embed_dim= dim_model, num_heads= heads_en,  )     # multihead attention    ## todo add dropout, LayerNORM
+        self.ffn_en = Feed_Forward_block( dim_model )                                            # feedforward block     ## todo dropout, LayerNorm
+        self.layer_norm1 = nn.LayerNorm( dim_model )
+        self.layer_norm2 = nn.LayerNorm( dim_model )
 
 
     def forward(self, in_ex, in_cat, first_block=True):
@@ -42,19 +42,27 @@ class Encoder_block(nn.Module):
             in_ex = self.embd_ex( in_ex )
             in_cat = self.embd_cat( in_cat )
             #in_pos = self.embd_pos( in_pos )
-
             #combining the embedings
-            out = in_ex + in_cat #+ in_pos         # (b,n,d)
+            out = in_ex + in_cat #+ in_pos                      # (b,n,d)
         else:
             out = in_ex
+
+        out = out.permute(1,0,2)                                # (n,b,d)  # print('pre multi', out.shape )
         
-        out = out.permute(1,0,2)               # (n,b,d)  # print('pre multi', out.shape )
-        #Multihead attention                            ## todo verify if E to passed as q,k,v
-        out, attn_wt = self.multi_en( out , out , out ) ## todo pass attention mask upper triangular
+        #Multihead attention                            
+        n,_,_ = out.shape
+        out = self.layer_norm1( out )                           # Layer norm
+        skip_out = out 
+        out, attn_wt = self.multi_en( out , out , out ,
+                                attn_mask=get_mask(seq_len=n))  # attention mask upper triangular
+        out = out + skip_out                                    # skip connection
 
         #feed forward
-        out = out.permute(1,0,2)               # (b,n,d)
+        out = out.permute(1,0,2)                                # (b,n,d)
+        out = self.layer_norm2( out )                           # Layer norm 
+        skip_out = out
         out = self.ffn_en( out )
+        out = out + skip_out                                    # skip connection
 
         return out
 
@@ -64,17 +72,15 @@ class Decoder_block(nn.Module):
     def __init__(self,dim_model ,total_in, heads_de,  ):
         super().__init__()
 
-        #interaction embedding
-        self.embd_in = nn.Embedding(  total_in , embedding_dim = dim_model )
+        self.embd_in    = nn.Embedding(  total_in , embedding_dim = dim_model )                  #interaction embedding
+        self.multi_de1  = nn.MultiheadAttention( embed_dim= dim_model, num_heads= heads_de  )  # M1 multihead for interaction embedding as q k v
+        self.multi_de2  = nn.MultiheadAttention( embed_dim= dim_model, num_heads= heads_de  )  # M2 multihead for M1 out, encoder out, encoder out as q k v
+        self.ffn_en     = Feed_Forward_block( dim_model )                                         # feed forward layer
 
-        # M1 multihead for interaction embedding as q k v
-        self.multi_de1 = nn.MultiheadAttention( embed_dim= dim_model, num_heads= heads_de  )  
+        self.layer_norm1 = nn.LayerNorm( dim_model )
+        self.layer_norm2 = nn.LayerNorm( dim_model )
+        self.layer_norm3 = nn.LayerNorm( dim_model )
 
-        # M2 multihead for M1 out, encoder out, encoder out as q k v
-        self.multi_de2 = nn.MultiheadAttention( embed_dim= dim_model, num_heads= heads_de  )  
-
-        # feed forward layer
-        self.ffn_en = Feed_Forward_block( dim_model )  
 
     def forward(self, in_in, en_out,first_block=True):
 
@@ -84,22 +90,34 @@ class Decoder_block(nn.Module):
             #in_pos = self.embd_pos( in_pos )
 
             #combining the embedings
-            out = in_in #+ in_cat #+ in_pos         # (b,n,d)
+            out = in_in #+ in_cat #+ in_pos                         # (b,n,d)
         else:
             out = in_in
         
-        out = out.permute(1,0,2)               # (n,b,d)# print('pre multi', out.shape )
+        out = out.permute(1,0,2)                                    # (n,b,d)# print('pre multi', out.shape )
+        n,_,_ = out.shape
 
-        #Multihead attention M1                                      ## todo verify if E to passed as q,k,v
-        out, attn_wt = self.multi_de1( out , out , out )             ## todo pass attention mask upper triangular
+        #Multihead attention M1                                     ## todo verify if E to passed as q,k,v
+        out = self.layer_norm1( out )
+        skip_out = out
+        out, attn_wt = self.multi_de1( out , out , out, 
+                                     attn_mask=get_mask(seq_len=n)) # attention mask upper triangular
+        out = skip_out + out                                        # skip connection
 
-        #Multihead attention M2                                      ## todo verify if E to passed as q,k,v
-        en_out = en_out.permute(1,0,2)           # (b,n,d)-->(n,b,d)
-        out, attn_wt = self.multi_de2( out , en_out , en_out )       ## todo pass attention mask upper triangular
+        #Multihead attention M2                                     ## todo verify if E to passed as q,k,v
+        en_out = en_out.permute(1,0,2)                              # (b,n,d)-->(n,b,d)
+        en_out = self.layer_norm2( en_out )
+        skip_out = out
+        out, attn_wt = self.multi_de2( out , en_out , en_out,
+                                    attn_mask=get_mask(seq_len=n))  # attention mask upper triangular
+        out = out + skip_out
 
         #feed forward
-        out = out.permute(1,0,2)               # (b,n,d)
-        out = self.ffn_en( out )                                     ## todo layernorm, skipconnection 
+        out = out.permute(1,0,2)                                    # (b,n,d)
+        out = self.layer_norm3( out )                               # Layer norm 
+        skip_out = out
+        out = self.ffn_en( out )                                    
+        out = out + skip_out                                        # skip connection
 
         return out
 
@@ -107,8 +125,14 @@ def get_clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
 
 
+def get_mask(seq_len):
+    ##todo add this to device
+    return torch.from_numpy( np.triu(np.ones((seq_len ,seq_len)), k=1).astype('bool'))
+
+
+
 class saint(nn.Module):
-    def __init__(self,dim_model,num_en, num_de ,heads_en, total_ex ,total_cat,total_in,heads_de):
+    def __init__(self,dim_model,num_en, num_de ,heads_en, total_ex ,total_cat,total_in,heads_de, ):
         super().__init__( )
 
         self.num_en = num_en
@@ -121,7 +145,7 @@ class saint(nn.Module):
     
     def forward(self,in_ex, in_cat,  in_in ):
         
-        # pass through each of the encoder blocks in sequence
+        ## pass through each of the encoder blocks in sequence
         first_block = True
         for x in range(self.num_en):
             if x>=1:
@@ -129,21 +153,20 @@ class saint(nn.Module):
             in_ex = self.encoder[x]( in_ex, in_cat ,first_block=first_block)
             in_cat = in_ex                                  # passing same output as q,k,v to next encoder block
 
-        # pass through each decoder blocks in sequence
+        
+        ## pass through each decoder blocks in sequence
         first_block = True
         for x in range(self.num_de):
             if x>=1:
                 first_block = False
             in_in = self.decoder[x]( in_in , en_out= in_ex, first_block=first_block )
 
-        # Output layer
+        ## Output layer
         in_in = torch.sigmoid( self.out( in_in ) )
         return in_in
 
 
-
-
-
+## forward prop on dummy data
 
 seq_len = 100
 total_ex = 1200
